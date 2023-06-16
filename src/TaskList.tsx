2a72d4035538eb445586b26a6cbaab3c80fe0c4c
Marcin Wiatr <devjoter@gmail.com>
2023-06-16 23:50:35 +0200

    added edit option

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -12,6 +12,8 @@ interface TaskListProps {
 const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
   const [taskList, setTaskList] = useState<Task[]>(tasks);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [editTaskId, setEditTaskId] = useState<number | null>(null);
+  const [editTaskTitle, setEditTaskTitle] = useState('');
 
   const addTask = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -32,13 +34,56 @@ const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
     setTaskList(updatedTaskList);
   };
 
+  const startEditTask = (taskId: number) => {
+    const taskToEdit = taskList.find((task) => task.id === taskId);
+    if (taskToEdit) {
+      setEditTaskId(taskId);
+      setEditTaskTitle(taskToEdit.title);
+    }
+  };
+
+  const cancelEditTask = () => {
+    setEditTaskId(null);
+    setEditTaskTitle('');
+  };
+
+  const saveTask = () => {
+    if (editTaskId === null || editTaskTitle.trim() === '') return;
+
+    const updatedTaskList = taskList.map((task) =>
+      task.id === editTaskId ? { ...task, title: editTaskTitle } : task
+    );
+
+    setTaskList(updatedTaskList);
+    setEditTaskId(null);
+    setEditTaskTitle('');
+  };
+
   return (
     <div>
       <ul>
         {taskList.map((task) => (
           <li key={task.id}>
-            {task.title}
-            <button onClick={() => removeTask(task.id)}>Usuń zadanie</button>
+            {editTaskId === task.id ? (
+              <input
+                type="text"
+                value={editTaskTitle}
+                onChange={(e) => setEditTaskTitle(e.target.value)}
+              />
+            ) : (
+              task.title
+            )}
+            {editTaskId === task.id ? (
+              <>
+                <button onClick={saveTask}>Zapisz</button>
+                <button onClick={cancelEditTask}>Anuluj</button>
+              </>
+            ) : (
+              <>
+                <button onClick={() => startEditTask(task.id)}>Edytuj zadanie</button>
+                <button onClick={() => removeTask(task.id)}>Usuń zadanie</button>
+              </>
+            )}
           </li>
         ))}
       </ul>
